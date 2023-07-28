a7821cd356e70da1acf43e37980ac82b6371ba1b
Kevin Allain <kevallain@gmail.com>
2023-07-28 16:44:43 +0100

    Changing size of fonts based on height of PianoRoll. Also changing opacity of rectangles. Might make further color changes.

--- src/components/MusicInterface/MusicInterface.js
+++ src/components/MusicInterface/MusicInterface.js
@@ -30,9 +30,9 @@ import PianoRoll from '../VisComponents/PianoRoll';
 
 const PITCH_QUERY_REGEX = /^$|(^(?!.*--)(?!-)([0-9]{1,2}|1[01][0-9]|12[0-7])(-([0-9]{1,2}|1[01][0-9]|12[0-7]))*(-?)$)/;
 // Test attributes
-const notes = [60, 61, 62, 64, 67, 55, 89];
-const occurrences = [0,1, 2, 4, 7, 7.5, 8];
-const durations = [0.5,1, 1, 2, 3, 5, 1];
+const notes = [60, 61, 62,63, 64, 67, 55, 77, 89,90, 82];
+const occurrences = [0,1, 2,3, 4, 7, 7.5, 8, 8,9, 13.25];
+const durations = [0.5,1, 1,1, 2, 3, 5, 2, 1,0.5, 1.82];
 
 
 const MusicInterface = () => {
--- src/components/VisComponents/PianoRoll.js
+++ src/components/VisComponents/PianoRoll.js
@@ -1,28 +1,33 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import MIDItoNote from '../MusicInterface/MIDItoNote.json'
+import './PianoRoll.css'
+
+  const noteToColor = {
+    A: "#FE2712",
+    "A#": "#FC600A",
+    B: "#FB9902",
+    C: "#FCCC1A",
+    "C#": "#FEFE33",
+    D: "#B2D732",
+    "D#": "#66B032",
+    E: "#347C98",
+    F: "#0247FE",
+    "F#": "#4424D6",
+    G: "#8601AF",
+    "G#": "#C21460",
+  };
+
+  
 
 const PianoRoll = ({ notes, occurrences, durations, width, height }) => {
   const svgRef = useRef(null);
 
-  const noteToColor = {
-    'A': '#FE2712',
-    'A#': '#FC600A',
-    'B': '#FB9902',
-    'C': '#FCCC1A',
-    'C#': '#FEFE33',
-    'D': '#B2D732',
-    'D#': '#66B032',
-    'E': '#347C98',
-    'F': '#0247FE',
-    'F#': '#4424D6',
-    'G': '#8601AF',
-    'G#': '#C21460',
-  }
+  let barHeight = height/20;
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
-    const margin = { top: 20, right: 30, bottom: 30, left: 50 };
+    const margin = { top: 10, right: 10, bottom: 20, left: 50 };
 
     const minNote = Math.min(...notes);
     const maxNote = Math.max(...notes);
@@ -31,76 +36,100 @@ const PianoRoll = ({ notes, occurrences, durations, width, height }) => {
     let maxTime = Number.MIN_VALUE;
     for (let i = 0; i < occurrences.length; i++) {
       const sum = occurrences[i] + durations[i];
-      if (sum > maxTime) { maxTime = sum; }
+      if (sum > maxTime) {
+        maxTime = sum;
+      }
     }
-    const xScale = d3.scaleLinear()
+    const xScale = d3
+      .scaleLinear()
       .domain([0, maxTime])
       .range([margin.left, width - margin.right]);
 
-    const yScale = d3.scaleLinear()
+    const yScale = d3
+      .scaleLinear()
       .domain([minNote, maxNote])
       .range([height - margin.bottom, margin.top]);
 
-    const barHeight = 10;
-    svg.append('rect')
-      .attr('x', 0)
-      .attr('y', 0)
-      .attr('width', width)
-      .attr('height', height)
-      .attr('fill', 'white');
+    // svg
+    //   .append("rect")
+    //   .attr('class','white_background')
+    //   .attr("x", 0)
+    //   .attr("y", 0)
+    //   .attr("width", width)
+    //   .attr("height", height)
+    //   .attr("fill", "white");
 
     // Generate an array of all integer occurrences
-    const occurrenceArray = Array.from({ length: maxTime - minOccurrence + 1 }, (_, i) => i + minOccurrence);
+    const occurrenceArray = Array.from(
+      { length: maxTime - minOccurrence + 1 },
+      (_, i) => i + minOccurrence
+    );
     console.log("occurrenceArray: ", occurrenceArray);
 
-
-    svg.selectAll('rect.bar')
+    svg
+      .selectAll("rect.bar")
       .data(notes)
       .enter()
-      .append('rect')
-      .attr('x', (d, i) => xScale(occurrences[i]))
-      .attr('y', (d) => yScale(d) - barHeight / 2)
-      .attr('width', (d, i) => xScale(durations[i] + occurrences[i]) - xScale(occurrences[i]))
-      .attr('height', barHeight)
-      .attr('fill', (d => noteToColor[MIDItoNote[d].replaceAll('s','').replace(/\d+/g, '')]))
-      .attr('stroke', 'black'); // Set the stroke attribute to black
+      .append("rect")
+      .attr("class", "bar") 
+      .attr("x", (d, i) => xScale(occurrences[i]))
+      .attr("y", (d) => yScale(d) - barHeight / 2)
+      .attr(
+        "width",
+        (d, i) => xScale(durations[i] + occurrences[i]) - xScale(occurrences[i])
+      )
+      .attr("height", barHeight)
+      .attr( "fill", (d) =>
+          noteToColor[MIDItoNote[d].replaceAll("s", "").replace(/\d+/g, "")]
+      )
+      .attr("stroke", "black")
+      .attr('opacity','0.65')
 
-    // TODO not working here...
     // Append vertical dotted lines for the x-axis values
-    svg.selectAll('line.vertical')
+    svg
+      .selectAll("line.vertical")
       .data(occurrenceArray)
       .enter()
-      .append('line')
-      .attr('class', 'vertical')
-      .attr('x1', (d) => xScale(d))
-      .attr('y1', margin.top)
-      .attr('x2', (d) => xScale(d))
-      .attr('y2', height - margin.bottom)
-      .attr('stroke', 'red') // Change the stroke color to red
-      .attr('stroke-width', 2) // Increase the stroke width to 2        
-      .attr('stroke-dasharray', '2,2'); // Dotted line style
-
+      .append("line")
+      .attr("class", "vertical")
+      .attr("x1", (d) => xScale(d))
+      .attr("y1", margin.top)
+      .attr("x2", (d) => xScale(d))
+      .attr("y2", height - margin.bottom)
+      .attr("stroke", "black")
+      .attr("stroke-width", 1)
+      .attr("stroke-dasharray", "2,2"); // Dotted line style
 
     const xAxis = d3.axisBottom(xScale).ticks(10);
-    const yAxis = d3.axisLeft(yScale).tickValues(notes).tickFormat((d, index) =>
-      MIDItoNote[d].replaceAll('s', '')
-    );
+    
+    const yAxis = d3
+      .axisLeft(yScale)
+      .tickValues(notes)
+      .tickFormat((d, index) => MIDItoNote[d].replaceAll("s", ""));
 
-    svg.append('g')
-      .attr('transform', `translate(0, ${height - margin.bottom})`)
+    const yAxisElement = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(yAxis);
+
+    yAxisElement
+      .selectAll("text")
+      .attr("dx", (d, i) => 
+        (MIDItoNote[d].indexOf('#')===-1 ? "-17px" : "3px")) // Apply negative offset to every alternate label
+      .attr("fill", (d) => (MIDItoNote[d].replaceAll("s", "").replace(/\d+/g, "").indexOf('#') !== -1)?'#000':
+          noteToColor[MIDItoNote[d].replaceAll("s", "").replace(/\d+/g, "")] || "#000" )
+      .attr("font-size",`${1.5 + Math.log(height*4)}px`) // size of font is ok at 8px if height is 150px.
+      ;
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
       .call(xAxis);
 
-    svg.append('g')
-      .attr('transform', `translate(${margin.left}, 0)`)
-      .call(yAxis)
-      .selectAll('text') // Select all tick labels
-      .attr('dx', (_, i) => (i % 2 === 0) ? '-12px' : '3px'); // Apply negative offset to every alternate label  
-      ;
-  }, [notes, occurrences, durations, width, height]);
 
-  return (
-    <svg ref={svgRef} width={width} height={height}></svg>
-  );
+  }, [notes, occurrences, durations, width, height, barHeight]);
+
+  return <svg className='svg_background' ref={svgRef} width={width} height={height}></svg>;
 };
 
 export default PianoRoll;
