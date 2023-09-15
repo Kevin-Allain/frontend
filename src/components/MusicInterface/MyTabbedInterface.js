7543cf43c8fcd8baa7e159a43f8f4b924e389b28
Kevin Allain <kevallain@gmail.com>
2023-09-15 19:05:38 +0100

    Work in progress for MyTabbedInterface to display content more interesting to the user.

--- src/components/MusicInterface/MyTabbedInterface.js
+++ src/components/MusicInterface/MyTabbedInterface.js
@@ -47,56 +47,116 @@ const MyTabbedInterface = ({
   ," - infoMusicList[2]: ",infoMusicList[2],", infoMusicList['2']: ",infoMusicList['2']
   ,", listSearchRes: ",listSearchRes);
 
+  // We will need a new structure!
+  // -> Merge by event name if the lognumber has SJA?
+  const mergedData = {};
 
+  infoMusicList.forEach((item) => {
+    const eventName = item["(E) Event Name"];
+    const trackNumber = item["Track #"];
+    const lognumber = item["lognumber"];
+    if (eventName) {
+      if (!mergedData[eventName]) {
+        mergedData[eventName] = { tracks: [], };
+      }
+      if (!mergedData[eventName].tracks[trackNumber]) {
+        mergedData[eventName].tracks[trackNumber] = [];
+      }
+      mergedData[eventName].tracks[trackNumber].push(item);
+    } else if (lognumber) {
+      if (!mergedData[lognumber]) {
+        mergedData[lognumber] = { tracks: [], };
+      }
+  
+      // Calculate the trackNumber based on the number of existing tracks
+      const nextTrackNumber = mergedData[lognumber].tracks.length;
+      
+      if (!mergedData[lognumber].tracks[nextTrackNumber]) {
+        mergedData[lognumber].tracks[nextTrackNumber] = [];
+      }
+  
+      mergedData[lognumber].tracks[nextTrackNumber].push(item);
+    }
+  });
+  const result = Object.values(mergedData);
+  console.log(result);
+
+  let keysEvents = Object.keys(mergedData)
+  let valsEvents = Object.values(mergedData)
+  let tracksForEvent = [];
+  for (var d in mergedData) {
+    const tracks = mergedData[d].tracks.filter((a) => typeof a !== 'undefined');    
+    // Use concat to flatten the second level arrays
+    const flattenedTracks = [].concat(...tracks);
+    tracksForEvent.push(flattenedTracks);
+  }
+    
+  console.log({keysEvents, tracksForEvent });
 
   return (
     <div className="flex h-[40rem] bg-gray-100">
       {/* Sidebar with recording tabs */}
       <div className="w-1/8 p-4 overflow-y-auto custom-scrollbar">
-          <h2 className="text-lg font-semibold mb-4">Recordings</h2>
-          <ul>
-            {listLogNumbers && listLogNumbers.sort((a,b)=> a-b ).map((recording) => (
-              <li
-                key={recording}
-                className={`text-sm cursor-pointer mb-2 ${
-                  activeRecording === recording ? "text-orange-500" : ""
-                }`}
-                onClick={() => handleRecordingClick(recording)}
-              >
-                {/* {(recording.length>30)? recording.substring(0,30)+'...':recording} {findMatchRecording(recording)}  */}
-                {recording.includes("SJA_") ?
-                  infoMusicList[findMatchRecording(recording)] ?
-                    (infoMusicList[findMatchRecording(recording)]['(E) Event Name'] 
-                      + ((infoMusicList[findMatchRecording(recording)]['Event Month'])?(' M'+infoMusicList[findMatchRecording(recording)]['Event Month']):'')
-                      + ' ' + infoMusicList[findMatchRecording(recording)]['Event Year'])
-                    : ('No match somehow for number: ' + findMatchRecording(recording))
-                : recording
-                }
-              </li>
-            ))}
-          </ul>
-        </div>
-      
+        <h2 className="text-lg font-semibold mb-4">Recordings</h2>
+        <ul>
+          {keysEvents &&
+            keysEvents
+              .sort((a, b) => a - b)
+              .map((recording) => (
+                <li
+                  key={recording}
+                  className={`text-sm cursor-pointer mb-2 ${
+                    activeRecording === recording ? "text-orange-500" : ""
+                  }`}
+                  onClick={() => handleRecordingClick(recording)}
+                >
+                  {/* {(recording.length>30)? recording.substring(0,30)+'...':recording} {findMatchRecording(recording)}  */}
+                  {/* {recording.includes("SJA_")
+                    ? infoMusicList[findMatchRecording(recording)]
+                      ? infoMusicList[findMatchRecording(recording)][
+                          "(E) Event Name" ] + (infoMusicList[findMatchRecording(recording)][ "Event Month" ]
+                          ? " M" + infoMusicList[findMatchRecording(recording)][ "Event Month" ] : "") +
+                        " " + infoMusicList[findMatchRecording(recording)][ "Event Year" ]
+                      : "No match somehow for number: " + findMatchRecording(recording) : recording} */}
+                  {recording}
+                </li>
+              ))}
+        </ul>
+      </div>
+
       {/* Sidebar with track tabs */}
       <div className="w-1/8 p-4 overflow-y-auto custom-scrollbar">
         <h2 className="text-lg font-semibold mb-4">Tracks</h2>
         {/* <> {"listTracks.length: "+listTracks.length+", listTracks[0]: "+listTracks[0]} </> */}
         <ul>
           {/* {activeRecording && recordingData[activeRecording].map((track) => ( <li key={track} className={`cursor-pointer mb-2 ${ activeTrack === track ? "text-orange-500" : "" }`} onClick={() => handleTrackClick(track)} > {track} </li> ))} */}
-          {listLogNumbers.length > 0 && listTracks && listTracks.length > 0 && listTracks.map((track, ndx) => {
-            return (track.includes(activeRecording)) ?
-              <li
-                key={track}
-                className={`text-sm cursor-pointer mb-2 ${activeTrack === track ? "text-orange-500" : ""}`}
-                onClick={() => handleTrackClick(track)}>
-                {activeRecording.includes("SJA_") ?
-                  infoMusicList[findMatchRecording(activeRecording)] ?
-                    (infoMusicList[findMatchRecording(activeRecording)]['Track Title']+' '+ infoMusicList[findMatchRecording(activeRecording)]['Track #']) : track.split('-')[1]
-                  : track.split('-')[1]}
-              </li>
-              : <></>
-          })
-          }
+          {tracksForEvent && activeRecording &&
+            tracksForEvent.length > 0 &&
+            tracksForEvent[keysEvents.indexOf(activeRecording)].map((a, i) =>
+              typeof a["(E) Event Name"] === "undefined" ? (
+                <li
+                  key={a + "" + i}
+                  className={`text-sm cursor-pointer mb-2 ${
+                    activeTrack === a + "" + i ? "text-orange-500" : ""
+                  }`}
+                  onClick={() => handleTrackClick(a + "" + i)}
+                >
+                  Track bgr
+                </li>
+              ) : (
+                <li
+                  key={a["Track Title"]}
+                  className={`text-sm cursor-pointer mb-2 ${
+                    activeRecording === a["Track Title"]
+                      ? "text-orange-500"
+                      : ""
+                  }`}
+                  onClick={() => handleRecordingClick(a["Track Title"])}
+                >
+                  {a["Track Title"]}
+                </li>
+              )
+            )}
         </ul>
       </div>
 
@@ -105,37 +165,43 @@ const MyTabbedInterface = ({
         {activeRecording && activeTrack && (
           <div>
             <h2 className="text-lg font-semibold mb-4">
-              {activeRecording} - {activeTrack.split('-')[1]}
+              {activeRecording} - {activeTrack.split("-")[1]}
             </h2>
 
             {/* Accordion for Recording and Track */}
             <div className="border rounded border-2 mb-[0.5rem]">
-              <MetadataAccordion 
-                title="Recording Interaction and Metadata" 
-                type={"recording"} 
-                content={listSearchRes[0].arrIdNotes[0]} 
-                info={activeRecording} 
-                findMatchRecording={findMatchRecording} 
-                infoMusicList={infoMusicList} 
+              <MetadataAccordion
+                title="Recording Interaction and Metadata"
+                type={"recording"}
+                content={listSearchRes[0].arrIdNotes[0]}
+                info={activeRecording}
+                findMatchRecording={findMatchRecording}
+                infoMusicList={infoMusicList}
               />
             </div>
             <div className="border rounded border-2 mb-[0.5rem]">
-              <MetadataAccordion 
-                title="Track Interaction" 
-                type={"track"} 
-                content={listSearchRes[0].arrIdNotes[0]} 
-                info={activeTrack} 
-                findMatchRecording={findMatchRecording} 
+              <MetadataAccordion
+                title="Track Interaction"
+                type={"track"}
+                content={listSearchRes[0].arrIdNotes[0]}
+                info={activeTrack}
+                findMatchRecording={findMatchRecording}
               />
             </div>
 
-
             {/* We should change TrackRes I think... */}
-            <TrackRes key={'Track_' + activeTrack} text={activeTrack} listSearchRes={listSearchRes.filter((a) => a.recording === activeTrack)}
-              formatAndPlay={formatAndPlay} getMusicInfo={getMusicInfo} infoMusicList={infoMusicList} setInfoMusicList={setInfoMusicList}
-              testPerformances={false} />
-
-
+            <TrackRes
+              key={"Track_" + activeTrack}
+              text={activeTrack}
+              listSearchRes={listSearchRes.filter(
+                (a) => a.recording === activeTrack
+              )}
+              formatAndPlay={formatAndPlay}
+              getMusicInfo={getMusicInfo}
+              infoMusicList={infoMusicList}
+              setInfoMusicList={setInfoMusicList}
+              testPerformances={false}
+            />
           </div>
         )}
       </div>
