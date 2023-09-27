36d6ef4682b1b72b5258478bad4b966c8e5239fe
Kevin Allain <kevallain@gmail.com>
2023-09-27 10:01:15 +0100

    Made the display prettier. We need to make tests to ensure the loading of data and its filtering is working as expected.

--- src/components/MusicInterface/MetadataAccordion.js
+++ src/components/MusicInterface/MetadataAccordion.js
@@ -44,87 +44,161 @@ const MetadataAccordion = ({
     // });
 
     return (
-        <div className="metadata-accordion ">
-            <div className="metadata-header flex" onClick={toggleAccordionRecording}>
-                <div className="metadata-title font-semibold">Recording Interaction and Metadata</div>
-                {expandedRecording ? (
-                    <FaAngleUp className="metadata-icon" />
-                ) : (
-                    <FaAngleDown className="metadata-icon" />
-                )}
-            </div>
-            {expandedRecording &&
-                (<div className="metadata-content">
-                    {infoMusicList !== null ? (
-                        infoMusicList.length === 0 ? (<AiOutlineLoading className='spin' size={'20px'} />) :
-                        findMatchRecording(recording) !== -1 ? (
-                            <div className='detailResultMeta'>
-                                <u>Info about recording:</u>
-                                <hr />
-                                {Object.entries(infoMusicList.filter(a => a.lognumber === recording)[0])
+      <div className="metadata-accordion ">
+        <div
+          className="metadata-header flex"
+          onClick={toggleAccordionRecording}
+        >
+          <div className="metadata-title font-semibold">
+            Recording Interaction and Metadata
+          </div>
+          {expandedRecording ? (
+            <FaAngleUp className="metadata-icon" />
+          ) : (
+            <FaAngleDown className="metadata-icon" />
+          )}
+        </div>
+        {expandedRecording && (
+          <div className="metadata-content">
+            {infoMusicList !== null ? (
+              infoMusicList.length === 0 ? (
+                <AiOutlineLoading className="spin" size={"20px"} />
+              ) : findMatchRecording(recording) !== -1 ? (
+                <div className="detailResultMeta">
+                  <u>Info about recording:</u>
+                  {/* Full display set for testing */}
+                  {/* {Object.entries(infoMusicList.filter(a => a.lognumber === recording)[0])
                                     .map(([key, value]) => (
                                         (value.length !== 0 ) ? <p key={key}> {key}: {value}</p> : <></>
                                     ))}
-                                <br/><hr /><br/>
-                                {/* TODO remove comments after testing. */}
-                                {Object.entries(infoMusicList.filter(a => a.lognumber === recording)[0])
-                                    .map(([key, value]) => (
-                                        (value.length !== 0 
-                                            // && (key === '(A/R/D) Event Type' || key === '(N) Named Artist(s)'
-                                            //     || key === '(E) Event Name' || key === '(Y) Date'
-                                            //     || key === 'Label' || key === 'AudioSource' || key === 'Musicians (instruments)'
-                                            // )
-                                        ) ?
-                                            <p key={key}> {key}: {value}</p>
-                                            : <></>
-                                    ))}
-                            </div>
-                        )
-                            : (<><div className='text-left'>No metadata about the recording</div><br /></>)
-                ) : <></>
-                    }
-                    <AnnotationSystem type={"recording"} recording={recording} />
-                    <EmbeddedWorkflowInteraction idCaller={content} typeCaller={"recording"} />
-                </div>)
-            }
-            <hr/>
-            <div className="metadata-header flex" onClick={toggleAccordionTrack}>
-                <div className="metadata-title font-semibold">Track Interaction and Metadata</div>
-                {expandedTrack ? (
-                    <FaAngleUp className="metadata-icon" />
-                ) : (
-                    <FaAngleDown className="metadata-icon" />
-                )}
-            </div>
-            {expandedTrack &&
-                (<div className="metadata-content">
-                    {infoMusicList !== null ? (
-                        infoMusicList.length === 0 ? (<AiOutlineLoading className='spin' size={'20px'} />) :
-                            infoMusicList.filter(a => a.lognumber === recording).filter(a => a['SJA ID'] === track.replace('-T','_')).length === 0 ? <><div className='text-left'>No metadata about the track (or a BGR. Track: {track})</div><br /></>
-                                : infoMusicList.filter(a => a.lognumber === recording).filter(a => a['SJA ID'] === track.replace('-T','_'))[0]['Track Title']
-                                // .map(a =>
-                                //     <div className='detailResultMeta'>
-                                //         <u>Info about track:</u>
-                                //         {Object.entries(a
-                                //             .filter(a => a.lognumber === recording)
-                                //             .filter(a => a['SJA ID'] === track.replace('-T','_')))[0]
-                                //             .map(([key, value]) => (
-                                //                 (value.length !== 0) ?
-                                //                     <p key={key}> {key}: {value}</p>
-                                //                     : <></>
-                                //             ))}
-                                //     </div>
-                                // )
-                    ) : <></>
-                    }
-                    {/* TODO this seems wrong...?! */}
-                    <AnnotationSystem type={"track"} track={track} />
-                    <EmbeddedWorkflowInteraction idCaller={content} typeCaller={"track"} />
-                </div>)
-            }
-
-
+                                <br/><hr /><br/> */}
+                  {/* We want to display attributes (if they exist)
+                                - (A/R/D) Event Type
+                                - (N) Named Artist(s)
+                                - (E) Event Name
+                                - (Y) Date
+                                - Label
+                                - AudioSource
+                                - Musicians (instruments)
+                                - Composition
+                                - Composer(s)
+                                - Observations
+                                */}
+                  {Object.entries(
+                    infoMusicList.filter((a) => a.lognumber === recording)[0]
+                  ).map(([key, value]) =>
+                    value.length !== 0 &&
+                    (key === "(A/R/D) Event Type" ||
+                      key === "(N) Named Artist(s)" ||
+                      key === "(E) Event Name" ||
+                      key === "(Y) Date" ||
+                      key === "Label" ||
+                      key === "AudioSource" ||
+                      key === "Musicians (instruments)" ||
+                      key === "Composition" ||
+                      key === "Composer(s)" ||
+                      key === "Observations") ? (
+                      <p key={key}>
+                        {" "}
+                        {key}: {value}
+                      </p>
+                    ) : (
+                      <></>
+                    )
+                  )}
+                </div>
+              ) : (
+                <>
+                  <div className="text-left">
+                    No metadata about the recording
+                  </div>
+                  <br />
+                </>
+              )
+            ) : (
+              <></>
+            )}
+            <AnnotationSystem type={"recording"} recording={recording} />
+            <EmbeddedWorkflowInteraction
+              idCaller={content}
+              typeCaller={"recording"}
+            />
+          </div>
+        )}
+        <hr />
+        <div className="metadata-header flex" onClick={toggleAccordionTrack}>
+          <div className="metadata-title font-semibold">
+            Track Interaction and Metadata
+          </div>
+          {expandedTrack ? (
+            <FaAngleUp className="metadata-icon" />
+          ) : (
+            <FaAngleDown className="metadata-icon" />
+          )}
         </div>
+        {/* We want to display attributes (if they exist)
+            - Track Title
+            - Track #
+            - Duration
+             */}
+        {expandedTrack && (
+          <div className="metadata-content">
+            <div className="detailResultMeta">
+              <u>Info about track:</u>
+              {infoMusicList !== null ? (
+                infoMusicList.length === 0 ? ( <AiOutlineLoading className="spin" size={"20px"} /> )
+                : infoMusicList
+                    .filter((a) => a.lognumber === recording)
+                    .filter((a) => a["SJA ID"] === track.replace("-T", "_"))
+                    .length === 0 ? (
+                  <>
+                    <div className="text-left">
+                      No metadata about the track (or a BGR. Track: {track})
+                    </div>
+                    <br />
+                  </>
+                ) : (
+                  Object.entries(
+                    infoMusicList
+                      .filter((a) => a.lognumber === recording)
+                      .filter(
+                        (a) => a["SJA ID"] === track.replace("-T", "_")
+                      )[0]
+                  ).map(([key, value]) =>
+                    value.length !== 0 &&
+                    (key === "Track Title" || key === "Track #" || key === "Duration") ? (
+                      <p key={key}> {" "} {key}: {value} </p>
+                    ) : (
+                      <></>
+                    )
+                  )
+                )
+              ) : (
+                // .map(a =>
+                //     <div className='detailResultMeta'>
+                //         <u>Info about track:</u>
+                //         {Object.entries(a
+                //             .filter(a => a.lognumber === recording)
+                //             .filter(a => a['SJA ID'] === track.replace('-T','_')))[0]
+                //             .map(([key, value]) => (
+                //                 (value.length !== 0) ?
+                //                     <p key={key}> {key}: {value}</p>
+                //                     : <></>
+                //             ))}
+                //     </div>
+                // )
+                <></>
+              )}
+            </div>
+            {/* TODO this seems wrong...?! */}
+            <AnnotationSystem type={"track"} track={track} />
+            <EmbeddedWorkflowInteraction
+              idCaller={content}
+              typeCaller={"track"}
+            />
+          </div>
+        )}
+      </div>
     );
 };
 
