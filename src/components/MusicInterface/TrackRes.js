ee08337cdf12e8fdb2aa1dadbf70d3e6a1b7ce23
Kevin Allain <kevallain@gmail.com>
2023-10-03 23:06:30 +0100

    Set a limit of samples to be loaded at once, with some buttons to navigate between them

--- src/components/MusicInterface/TrackRes.js
+++ src/components/MusicInterface/TrackRes.js
@@ -10,6 +10,7 @@ import {
 } from "../../utils/HandleApi";
 import AnnotationSystem from '../Annotation/AnnotationSystem';
 import EmbeddedWorkflowInteraction from '../Workflow/EmbeddedWorkflowInteraction';
+import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io'
 
 const TrackRes = ({
     text,
@@ -22,31 +23,44 @@ const TrackRes = ({
     setInfoMusicList,
     testPerformances = false
 }) => {
-    console.log("TrackRes - ",{
-        text,
-        infoMusicList,
-        listSearchRes,
-        formatAndPlay,
-        getMusicInfo,
-        setInfoMusicList,
-        testPerformances    
-    })
+    console.log("TrackRes - ",{ text, infoMusicList, listSearchRes, formatAndPlay, getMusicInfo, setInfoMusicList, testPerformances     })
+
+    const [startIndex, setStartIndex] = useState(0);
+    const itemsPerPage = 20;
+
+    const handleNextRange = () => {
+        if ((startIndex + itemsPerPage) <= listSearchRes.length) setStartIndex(startIndex + itemsPerPage);
+    };
+
+    const handlePrevRange = () => {
+        setStartIndex(Math.max(0, startIndex - itemsPerPage));
+    };
+
+    const currentItems = listSearchRes.slice(startIndex, startIndex + itemsPerPage);
+
+
+
     return (
       <div className="trackres" key={text}>
         {/* <div className="texttrackres"> <h3>Track: {text} </h3> </div> */}
-        {/* <div className='iconTracksInteractions'>
-                <AnnotationSystem type={"track"} info={text} />
-                <EmbeddedWorkflowInteraction idCaller={listSearchRes[0].arrIdNotes[0]} typeCaller={"track"} />
-            </div> */}
+        {/* <div className='iconTracksInteractions'> <AnnotationSystem type={"track"} info={text} /> <EmbeddedWorkflowInteraction idCaller={listSearchRes[0].arrIdNotes[0]} typeCaller={"track"} /> </div> */}
         {testPerformances ? (
           <></>
         ) : (
           <>
             {listSearchRes.length} samples. <br />
-            {listSearchRes.map((item, i) => (
+            { listSearchRes.length > itemsPerPage &&
+            <div className="pagination-buttons flex">
+                <IoIosArrowBack className='icon' onClick={handlePrevRange} disabled={startIndex === 0}/> | {startIndex}-{Math.min(listSearchRes.length,startIndex + itemsPerPage)} |
+                {/* Previous</button> */}
+                <IoIosArrowForward className='icon' onClick={handleNextRange} disabled={(startIndex + itemsPerPage) >= listSearchRes.length}/>
+                {/* Next</button> */}
+            </div>
+            }
+            {currentItems.map((item, i) => (
               <div className="border-4 border-solid rounded">
                 <div className="text-left mx-[1rem] my-[0.25rem] ">
-                  Sample {i}{" "}
+                  Sample {startIndex+i}{" "}
                 </div>
                 {/* {item.track}  */}
                 <SampleRes
@@ -71,6 +85,7 @@ const TrackRes = ({
                 />
               </div>
             ))}
+
           </>
         )}
       </div>
